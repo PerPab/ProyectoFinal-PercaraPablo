9d8f73feff57c86d4ca1610f1302bac8197b0b99
Pablo <percara.pablo@gmail.com>
2023-01-16 18:14:29 -0300

    validaciones

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@ window.addEventListener('load', mostrarPrincipal)
 let listaTerrenos = [
     {
         titulo: 'En Oferta',
-        precio: '95.000',
+        precio: 95000,
         imagen_opcional_1: './img/carousel-1/card1-2.png',
-        imagen_opcional_2: './img/carousel-1/card1-3.png',
         ubicacion: 'Posidonius',
         descripcion: 'Magnífico cráter de 95km cuadrados, a metros de los restos del Luna 21. Ideal para familia numerosa. Espacio para quincho y parrilla'
 
@@ -14,9 +13,8 @@ let listaTerrenos = [
 
     {
         titulo: 'En Oferta',
-        precio: '80.000',
+        precio: 80000,
         imagen_opcional_1: './img/carousel-2/card2-2.png',
-        imagen_opcional_2: './img/carousel-2/card2-3.png',
         ubicacion: 'Fabbroni',
         descripcion: 'Pequeño y acogedor cráter de 10km cuadrados. Ubicado cerca de los restos de la misión Apollo 17. Date el gusto de fotografiar a la familia Armstrong!'
 
@@ -24,9 +22,8 @@ let listaTerrenos = [
 
     {
         titulo: 'En Oferta',
-        precio: '79.000',
+        precio: 105000,
         imagen_opcional_1: './img/carousel-3/card-3-2.png',
-        imagen_opcional_2: './img/carousel-3/card-3-3.png',
         ubicacion: 'Lacus Bonitatis',
         descripcion: 'Si vivir en un cráter no es lo tuyo te traemos ésta planicie de 92km cuadrados. Oportunidad para emprendedores cancha de Paddle.'
 
@@ -34,9 +31,8 @@ let listaTerrenos = [
 
     {
         titulo: 'Oportunidad',
-        precio: '50.000',
+        precio: 50000,
         imagen_opcional_1: './img/1601.png',
-        imagen_opcional_2: './img/1601.png',
         ubicacion: 'Lado oscuro de la luna',
         descripcion: 'Esta propiedad a precio regalado no tiene desperdicio, ubicada en el lado oscuro de la luna un lugar ideal para aquellos que son fotosensibles.'
 
@@ -63,9 +59,6 @@ function agregarTerreno(){
                     <div class="carousel-item active">
                         <img id="img-2" class="imagen" src=${terreno.imagen_opcional_1} alt="imagen de un terreno">
                     </div>
-                    <div class="carousel-item">
-                        <img id="img-3" class="imagen" src=${terreno.imagen_opcional_2} alt="imagen de un terreno">
-                    </div>
                 </div>
             </div>
             <p class="card-text" id="ubicacion"><b>${terreno.ubicacion}</b></p>
@@ -80,28 +73,55 @@ function agregarTerreno(){
     });
 }
 function cargarLista(){
-    
+    let formulario = document.getElementById('publicar');
     let titulo_terreno = document.getElementById('titulo_terreno');
     let precio_terreno = document.getElementById('precio_terreno');
     let imagen_1_terreno = document.getElementById('img-2-terreno');
-    let imagen_2_terreno = document.getElementById('img-3-terreno');
     let ubicacion_terreno = document.getElementById('ubicacion_terreno');
     let descripcion_terreno = document.getElementById('descripcion_terreno');
-    
-    let terreno = {
-        titulo: titulo_terreno.value, 
-        precio: precio_terreno.value,
-        imagen_opcional_1: imagen_1_terreno.value,
-        imagen_opcional_2: imagen_2_terreno.value,
-        ubicacion: ubicacion_terreno.value,
-        descripcion: descripcion_terreno.value
+
+    if((titulo_terreno.value).trim() == ''){
+        titulo_terreno.setAttribute('placeholder','Este campo no puede quedar vacio!')
+        
+    }else if((precio_terreno.value).trim()==''){
+        precio_terreno.setAttribute('placeholder','Este campo no puede quedar vacio!')
+       
+    }else if((ubicacion_terreno.value).trim()==''){
+        ubicacion_terreno.setAttribute('placeholder','Este campo no puede quedar vacio!')
+
+    }else if((imagen_1_terreno.value).trim()==''){
+        imagen_1_terreno.setAttribute('placeholder','Este campo no puede quedar vacio!')
+     
+    }else if((descripcion_terreno.value).trim()==''){
+        descripcion_terreno.setAttribute('placeholder','Este campo no puede quedar vacio!') 
     }
-    listaTerrenos.push(terreno);
-    mostrarPrincipal()  
+    else{
+        let terreno = {
+            titulo: titulo_terreno.value, 
+            precio: Number(precio_terreno.value),
+            imagen_opcional_1: imagen_1_terreno.value,
+            ubicacion: ubicacion_terreno.value,
+            descripcion: descripcion_terreno.value
+        }
+        listaTerrenos.push(terreno);
+        formulario.reset()
+        mostrarPrincipal()  
+    }
 }
 
-
-    
+function ordenarMenorAmayor(){
+    listaTerrenos.sort(function (a, b) {
+        if (a.precio > b.precio) {
+          return 1;
+        }
+        if (a.precio < b.precio) {
+          return -1;
+        }
+        return 0;
+      });
+      mostrarPrincipal()
+}
+  
 
 function mostrarPrincipal(){
     contenedor.classList.remove('oculta')
@@ -142,22 +162,22 @@ btn_info.addEventListener('click', mostrarInfo)
 let btn_publicacion = document.getElementById('btn_prop')
 btn_publicacion.addEventListener('click', MostrarPublicar)
 
-let btn_comprar = document.getElementsByName('btn_comprar_name')
+let btn_comprar = document.getElementsById('btn_comprar_id')
 for(let boton of btn_comprar){
     boton.addEventListener('click', mostrarConsulta)
 }
-
-
-
-
- 
-
-
 
 let btn_publicar = document.getElementById("btn_publicar").addEventListener("click", function(event){
     event.preventDefault()
     cargarLista()
 });
+
+let btn_ordenar = document.getElementById('ordenar')
+btn_ordenar.addEventListener('click', ordenarMenorAmayor)
+
+
+
+
 
 
 
